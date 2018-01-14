f805cc487d1464f002086ea0df19516f59ee63c2
Gaby Kupfer <gaby@itsme3d.com>
2018-01-14 18:08:20 -0500

    Fixed some paths, good to go!

---
+++ dev/scripts/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                <h1>Ah, to be a new application!</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
